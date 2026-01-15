c669e4dcc1e96e98f7f03e31efad48ebc6aa412d
Rajendra Tamang <tamangrajendra0523@gmail.com>
2026-01-15 19:11:46 +0545

    feat:socket creataed

---
+++ server/socket/index.js
@@ -0,0 +1,11 @@
+const socketHandler = (io) => {
+  io.on("connection", (socket) => {
+    console.log("🟢 Client connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("🔴 Client disconnected:", socket.id);
+    });
+  });
+};
+
+module.exports = socketHandler;
